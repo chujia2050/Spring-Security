e995e0863ddfdd7e494b92f8b76d8d72a933dc66
chenc <cjchen122720@gmail.com>
2023-10-12 17:26:27 -0700

    Initial commit

---
+++ demosecurity/src/main/java/com/example/demosecurity/controller/DemoController.java
@@ -0,0 +1,30 @@
+package com.example.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String showHome(){
+
+        return "home";
+    }
+
+    @GetMapping("/leaders")
+    public String showLeaders(){
+
+        return "leaders";
+    }
+
+    @GetMapping("/systems")
+    public String showSystems(){
+
+        return "systems";
+    }
+
+}
